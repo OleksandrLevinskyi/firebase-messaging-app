29b1c9074b435bc315872dcfdb13ddf34da7b5f3
oleksandrlevinskyi <aleksandrlevinskyi@gmail.com>
2024-03-24 04:14:42 -0400

    create a firestore doc for new users

--- src/components/AuthStateHandler.js
+++ src/components/AuthStateHandler.js
@@ -2,13 +2,22 @@ import {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {onAuthStateChanged} from 'firebase/auth';
 import {auth} from "../firebase";
+import {UserManager} from "../managers/UserManager";
 
 export const AuthStateHandler = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        return onAuthStateChanged(auth, (user) => {
-            if (user) {
+        return onAuthStateChanged(auth, async (loggedInUser) => {
+            if (loggedInUser) {
+                const uid = loggedInUser.uid;
+                const user = await UserManager.get(uid);
+
+                if (user === null) {
+                    // if it is a new user, create a firestore doc
+                    await UserManager.create({id: uid, name: loggedInUser.displayName, image: loggedInUser.photoURL})
+                }
+
                 navigate('/messages');
             } else {
                 navigate('/');
