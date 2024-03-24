abb0b000384d493fd49ffd94230ab0297e5b801d
oleksandrlevinskyi <aleksandrlevinskyi@gmail.com>
2024-03-23 23:56:34 -0400

    add input field

--- src/pages/MessagesPage.js
+++ src/pages/MessagesPage.js
@@ -1,8 +1,10 @@
 import {MessageCard} from "../components/MessageCard";
-import {Box, Typography} from "@mui/material";
-import { Timestamp } from "firebase/firestore";
+import {Box, IconButton, TextField, Typography} from "@mui/material";
+import {Timestamp} from "firebase/firestore";
+import {Send} from "@mui/icons-material";
+import {useState} from "react";
 
-const messages = [
+const dbMessages = [
     {
         id: 1,
         name: 'Alice',
@@ -22,8 +24,30 @@ const messages = [
 ];
 
 export const MessagesPage = () => {
+    const [messages, setMessages] = useState(dbMessages);
+    const [inputText, setInputText] = useState('');
+
+    const handleSend = () => {
+        const newMessage = {
+            id: messages.length + 1,
+            name: 'You',
+            text: inputText,
+            image: 'https://example.com/path-to-your-avatar.jpg',
+            isAuthor: true,
+            timestamp: Timestamp.fromDate(new Date())
+        };
+        setMessages([...messages, newMessage]);
+        setInputText('');
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSend();
+        }
+    };
+
     return (
-        <Box sx={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+        <Box sx={{display: 'flex', flexDirection: 'column', width: {sm: '90%', md: '50%'}, margin: 'auto'}}>
             <Typography variant="h3" color="text.primary" textAlign="center" sx={{my: 3}}>
                 Messages
             </Typography>
@@ -32,6 +56,23 @@ export const MessagesPage = () => {
                     <MessageCard key={index} message={message}/>
                 ))
             }
+            <TextField
+                variant="outlined"
+                fullWidth
+                placeholder="Send a message..."
+                sx={{mx: 'auto', mt: 5, width: '90%'}}
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+                onKeyDown={handleKeyDown}
+                InputProps={{
+                    endAdornment: (
+                        <IconButton onClick={handleSend}>
+                            <Send/>
+                        </IconButton>
+                    ),
+                    style: { borderRadius: '20px' },
+                }}
+            />
         </Box>
     );
 }
