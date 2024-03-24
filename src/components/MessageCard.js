ddf8854d6c56bdab3b06952fa029deb9170968cb
oleksandrlevinskyi <aleksandrlevinskyi@gmail.com>
2024-03-23 22:42:02 -0400

    implement different styling for the authored messages

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,7 +1,7 @@
 import {Avatar, Card, CardContent, Typography} from "@mui/material";
-import {blue} from "@mui/material/colors";
+import {blue, green} from "@mui/material/colors";
 
-export const MessageCard = ({name, message, avatarSrc}) => {
+export const MessageCard = ({name, message, avatarSrc, isAuthor = false}) => {
     return (
         <Card sx={{
             display: 'flex',
@@ -9,12 +9,19 @@ export const MessageCard = ({name, message, avatarSrc}) => {
             mb: 2,
             width: 'fit-content',
             background: 'transparent',
-            boxShadow: 'none'
+            boxShadow: 'none',
+            flexDirection: isAuthor ? 'row-reverse' : 'row',
+            alignSelf: isAuthor ? 'flex-end' : 'flex-start',
         }}>
             <Avatar sx={{mx: 2}} src={avatarSrc} alt={name}/>
-            <CardContent sx={{flex: '1 1 auto', backgroundColor: blue[100], borderRadius: '20px'}}>
+            <CardContent sx={{
+                flex: '1 1 auto',
+                backgroundColor: isAuthor ? green[100] : blue[100],
+                borderRadius: '20px',
+                textAlign: isAuthor ? 'right' : 'left'
+            }}>
                 <Typography variant="subtitle1" color="text.secondary">
-                    {name}
+                    {isAuthor ? 'Me' : name}
                 </Typography>
                 <Typography variant="body2" color="text.primary">
                     {message}
--- src/pages/MessagesPage.js
+++ src/pages/MessagesPage.js
@@ -1,30 +1,35 @@
 import {MessageCard} from "../components/MessageCard";
-import {Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 
 const messages = [
     {
         id: 1,
         name: 'Alice',
         text: 'Hey, how are you?',
-        image: 'https://example.com/path-to-image-of-alice.jpg'
+        image: 'https://example.com/path-to-image-of-alice.jpg',
+        isAuthor: false
     },
     {
         id: 2,
         name: 'Bob',
         text: 'I\'m good! How about you?',
-        image: 'https://example.com/path-to-image-of-bob.jpg'
+        image: 'https://example.com/path-to-image-of-bob.jpg',
+        isAuthor: true
     }
 ];
 
 export const MessagesPage = () => {
     return (
-        <>
+        <Box sx={{display: 'flex', flexDirection: 'column', width: '100%'}}>
             <Typography variant="h3" color="text.primary" textAlign="center" sx={{my: 3}}>
                 Messages
             </Typography>
-            {messages.map((msg, index) => (
-                <MessageCard key={index} name={msg.name} message={msg.text} avatarSrc={msg.avatarSrc}/>
-            ))}
-        </>
+            {
+                messages.map((msg, index) => (
+                    <MessageCard key={index} name={msg.name} message={msg.text} avatarSrc={msg.avatarSrc}
+                                 isAuthor={msg.isAuthor}/>
+                ))
+            }
+        </Box>
     );
 }
